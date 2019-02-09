1faff239d5182834fe67b29dd6a2ca9f7d8758ca
ohbarye <over.rye@gmail.com>
2019-02-09 22:29:05 +0900

    Reorganize package.json

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
 import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
 
 export default {
   input: './src/index.ts',
   output: [
     {
-      file: 'dist/js/react-kana-provider.js',
+      file: pkg.module,
       format: 'esm',
     },
   ],
