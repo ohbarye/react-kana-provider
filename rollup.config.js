812d66c249375be78666e897d9133dd2edfafd7e
ohbarye <over.rye@gmail.com>
2019-02-09 21:29:30 +0900

    Set up rollup

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: './src/index.ts',
+  output: [
+    {
+      file: 'dist/js/react-kana-provider.js',
+      format: 'umd',
+    },
+  ],
+  plugins: [
+    typescript(),
+  ]
+}
